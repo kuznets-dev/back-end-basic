73c1e1f08954e12973d1c56c432fbc4e8ba5443c
Ilya Kuznetsov <i.kuzntesov.w@gamil.com>
2021-05-31 18:54:09 +0300

    Create server

---
+++ app.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import tasksGET from './routes/tasks.get.js';
+import taskPOST from './routes/task.post.js';
+// import taskPATCH from './routes/task.patch.js';
+// import taskDELETE from './routes/task.delete.js';
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.json());
+app.use(tasksGET);
+app.use(taskPOST);
+// app.use(taskPATCH);
+// app.use(taskDELETE);
+
+app.listen(PORT, () => {
+    console.log(`Server started...`);
+})
