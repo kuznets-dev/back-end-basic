f5614d9bb9561e7fb91b17f6a07b08ccec09b5ca
Ilya Kuznetsov <i.kuzntesov.w@gamil.com>
2021-05-31 18:54:47 +0300

    Create post request

---
+++ routes/task.post.js
@@ -0,0 +1,27 @@
+import { Router } from 'express';
+import fs from 'fs';
+import { v4 as uuidv4 } from 'uuid'
+
+const router = Router();
+
+router.post('/task', (req, res) => {
+    
+    if (!req.body) return res.statusCode(400);
+    const body = req.body
+    const task = {uuid: uuidv4(), ...body , createdAt: Date.now()};
+
+    fs.readFile('tasks.json', (err, data) => {
+        if(err){
+            throw err;
+        }
+        const tasks = JSON.parse(data);
+        
+        tasks.push(task);
+        data = JSON.stringify(tasks, null, 2);
+        fs.writeFileSync("tasks.json", data);
+    });
+    
+    res.send(req.body)
+})
+
+export default router;
