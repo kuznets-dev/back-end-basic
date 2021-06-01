4e783474fc7f9d04dc15683e3367ec12022f1a26
Ilya Kuznetsov <i.kuzntesov.w@gamil.com>
2021-06-01 17:49:39 +0300

    Add patch request

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import tasksGET from './routes/tasks.get.js';
 import taskPOST from './routes/task.post.js';
-// import taskPATCH from './routes/task.patch.js';
+import taskPATCH from './routes/task.patch.js';
 import taskDEL from './routes/task.delete.js';
 
 const PORT = process.env.PORT || 3001;
@@ -10,7 +10,7 @@ const app = express();
 app.use(express.json());
 app.use(tasksGET);
 app.use(taskPOST);
-// app.use(taskPATCH);
+app.use(taskPATCH);
 app.use(taskDEL);
 
 app.listen(PORT, () => {
