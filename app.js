f47acddab068b0265715e06afb73a2150fda5ddb
Ilya Kuznetsov <i.kuzntesov.w@gamil.com>
2021-06-01 16:28:12 +0300

    Add delete request

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ import express from 'express';
 import tasksGET from './routes/tasks.get.js';
 import taskPOST from './routes/task.post.js';
 // import taskPATCH from './routes/task.patch.js';
-// import taskDELETE from './routes/task.delete.js';
+import taskDEL from './routes/task.delete.js';
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(tasksGET);
 app.use(taskPOST);
 // app.use(taskPATCH);
-// app.use(taskDELETE);
+app.use(taskDEL);
 
 app.listen(PORT, () => {
     console.log(`Server started...`);
