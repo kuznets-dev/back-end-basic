f29b4880a25dc7a0adf052b3afa21035ff582434
Ilya Kuznetsov <i.kuzntesov.w@gamil.com>
2021-06-01 16:27:00 +0300

    Refactoring code

--- routes/task.post.js
+++ routes/task.post.js
@@ -1,19 +1,19 @@
 import { Router } from 'express';
 import fs from 'fs';
-import { v4 as uuidv4 } from 'uuid'
+import { v4 as uuidv4 } from 'uuid';
 
 const router = Router();
 
 router.post('/task', (req, res) => {
     
-    if (!req.body) return res.statusCode(400);
+    if (!req.body) return res.status(400).send('Something wrong');
+
     const body = req.body
     const task = {uuid: uuidv4(), ...body , createdAt: Date.now()};
 
     fs.readFile('tasks.json', (err, data) => {
-        if(err){
-            throw err;
-        }
+        if (err) {return res.status(400).send(err)}
+
         const tasks = JSON.parse(data);
         
         tasks.push(task);
