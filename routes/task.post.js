3af3ca17324656e1c9d9de932eea1e9af7b919e4
Ilya Kuznetsov <i.kuzntesov.w@gamil.com>
2021-06-01 17:51:13 +0300

    Refactoring code

--- routes/task.post.js
+++ routes/task.post.js
@@ -12,16 +12,15 @@ router.post('/task', (req, res) => {
     const task = {uuid: uuidv4(), ...body , createdAt: Date.now()};
 
     fs.readFile('tasks.json', (err, data) => {
+        
         if (err) {return res.status(400).send(err)}
 
         const tasks = JSON.parse(data);
-        
         tasks.push(task);
-        data = JSON.stringify(tasks, null, 2);
-        fs.writeFileSync("tasks.json", data);
+        fs.writeFileSync('tasks.json', JSON.stringify(tasks, null, 2));
     });
     
-    res.send(req.body)
+    res.send({ msg: 'New task was created', body });
 })
 
 export default router;
