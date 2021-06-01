46e3e495659261eaaa8023265c9dce550701d81d
Ilya Kuznetsov <i.kuzntesov.w@gamil.com>
2021-06-01 11:40:33 +0300

    Refactoring code

--- routes/tasks.get.js
+++ routes/tasks.get.js
@@ -3,15 +3,17 @@ import fs from 'fs';
 
 const router = Router();
 
-router.get('/tasks', (req, res) => {
-    fs.readFile('tasks.json', (err, data) => {
-        if(err){
-            throw err;
-        }
-        const tasks = JSON.parse(data)
-        res.send(tasks  )
-    })
+router.get('/tasks', async(req, res) => {
+    try {
+        fs.readFile('tasks.json', (err, data) => {
+            const tasks = JSON.parse(data);
+            res.send(tasks);
+        });
+    }
+    catch (err) {
+        console.log(err);
+        res.status(404).json(err);
+    }
 })
-
 
 export default router;
