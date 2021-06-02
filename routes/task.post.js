0cd9a0f5e04661826bf0e7072536379200890ca9
Ilya Kuznetsov <i.kuzntesov.w@gamil.com>
2021-06-02 13:20:28 +0300

    Add express-validator and refactoring code

--- routes/task.post.js
+++ routes/task.post.js
@@ -1,12 +1,20 @@
 import { Router } from 'express';
 import fs from 'fs';
 import { v4 as uuidv4 } from 'uuid';
+import { body, validationResult } from 'express-validator';
 
 const router = Router();
 
-router.post('/task', (req, res) => {
-    
-    if (!req.body) return res.status(400).send('Something wrong');
+router.post('/task',
+    body('name').isString().isLength({ min: 1 }),
+    body('done').isBoolean(),
+    (req, res) => {
+
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+        return res.status(400).send(errors.array());
+    }
 
     const body = req.body
     const task = {uuid: uuidv4(), ...body , createdAt: Date.now()};
