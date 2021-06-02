fcf94cdf5045e10cb53b3d7ae801be2db3ce93e1
Ilya Kuznetsov <i.kuzntesov.w@gamil.com>
2021-06-02 13:45:17 +0300

    Add express-validator and refactoring code

--- routes/task.patch.js
+++ routes/task.patch.js
@@ -1,17 +1,30 @@
 import { Router } from 'express';
 import fs from 'fs';
+import { body, validationResult } from 'express-validator';
 
 const router = Router();
 
-router.patch('/task/:id', (req, res) => {
+router.patch('/task/:id',
+    body('name').isString().isLength({ min: 1 }),
+    body('done').isBoolean(),
+    (req, res) => {
+
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+        return res.status(422).send(errors.array());
+    }
+        
+    const id = req.params.id;
+
     fs.readFile('tasks.json', (err, data) => {
-
-        if (err) {return res.status(400).send('Task not created')};
-
-        const id = req.params.id;
         const task = req.body;
         const tasks = JSON.parse(data);
+
         const index = tasks.findIndex(task => task.uuid === id);
+        
+        if (index === -1) {return res.status(400).send('Task not found')};
+
         tasks[index] = { ...tasks[index], name: task.name, done: task.done };
 
         fs.writeFileSync('tasks.json', JSON.stringify(tasks, null, 2));
