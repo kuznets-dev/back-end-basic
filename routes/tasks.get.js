68ac70211df7658a09070fa565db4e367deb3a5a
Ilya Kuznetsov <i.kuzntesov.w@gamil.com>
2021-06-02 12:47:47 +0300

    Add express-validator and refactoring code

--- routes/tasks.get.js
+++ routes/tasks.get.js
@@ -1,16 +1,37 @@
 import { Router } from "express";
 import fs from 'fs';
+import { query, validationResult } from 'express-validator';
 
 const router = Router();
 
-router.get('/tasks', (req, res) => {
+router.get('/tasks', 
+    query('filterBy').isString().optional({ checkFalsy: true }),
+    query('orderBy').isString().optional({ checkFalsy: true }),
+    (req, res) => {
+    const errors = validationResult(req);
 
+    if (!errors.isEmpty()) {
+        return res.status(400).send(errors.array());
+    }
+    
     fs.readFile('tasks.json', (err, data) => {
         
-        if (err) {return res.status(400).send(err)};
+        if (err) {return res.send(err)};
 
+        const { filterBy, orderBy } = req.query;
         const tasks = JSON.parse(data);
-        res.send(tasks);
+
+        let newTasks = tasks;
+
+        if (filterBy) {
+            newTasks = tasks.filter(task => filterBy ? task.done.toString() === filterBy : task);
+        }
+
+        if (orderBy) {
+            newTasks = newTasks.sort((a, b) => orderBy === 'asc' ? a.createdAt - b.createdAt : b.createdAt - a.createdAt);
+        }
+
+        res.send(newTasks);
     })
 });
 
