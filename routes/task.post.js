843844f87266301105d39c617b37629736e7e3af
Ilya Kuznetsov <i.kuzntesov.w@gamil.com>
2021-06-03 16:00:03 +0300

    Changed post request for db

--- routes/task.post.js
+++ routes/task.post.js
@@ -1,34 +1,29 @@
-import { Router } from 'express';
-import fs from 'fs';
-import { v4 as uuidv4 } from 'uuid';
-import { body, validationResult } from 'express-validator';
+const { Router } = require ('express');
+const { Task } = require ('../models');
+const { body, validationResult } = require ('express-validator');
 
 const router = Router();
 
 router.post('/task',
     body('name').isString().isLength({ min: 1 }),
     body('done').isBoolean(),
-    (req, res) => {
+    async (req, res) => {
+        
+        const { name, done } = req.body;
+    
+        try {
+        const errors = validationResult(req);
 
-    const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).send(errors.array());
+        }
 
-    if (!errors.isEmpty()) {
+        const task = await Task.create({ name, done });
+        return res.json(task); 
+        
+    } catch (err) {
         return res.status(400).send(errors.array());
     }
-
-    const body = req.body
-    const task = {uuid: uuidv4(), ...body , createdAt: Date.now()};
-
-    fs.readFile('tasks.json', (err, data) => {
-        
-        if (err) {return res.status(400).send(err)}
-
-        const tasks = JSON.parse(data);
-        tasks.push(task);
-        fs.writeFileSync('tasks.json', JSON.stringify(tasks, null, 2));
-    });
-    
-    res.send({ msg: 'New task was created', body });
 })
 
-export default router;
+module.exports = router;
