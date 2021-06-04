dc0d66b99ada2efd16580b2d8d183b53f3e1a1b5
Ilya Kuznetsov <i.kuzntesov.w@gamil.com>
2021-06-04 10:58:08 +0300

    Add Error Handler

--- routes/task.delete.js
+++ routes/task.delete.js
@@ -1,12 +1,13 @@
 const { Router } = require ('express');
 const { Task } = require ('../models');
 const { param, validationResult } = require ('express-validator');
+const { ErrorHandler } = require('../error');
 
 const router = Router();
 
 router.delete('/task/:uuid',
     param('uuid').isUUID(),
-    async (req, res) => {
+    async (req, res, next) => {
 
         const uuid = req.params.uuid;
         
@@ -14,17 +15,20 @@ router.delete('/task/:uuid',
             const errors = validationResult(req);
 
             if (!errors.isEmpty()) {
-                return res.status(400).send(errors.array());
+                throw new ErrorHandler(400, errors.array());
             }
 
             const task = await Task.findOne({ where: { uuid } });
 
+            if (!task) {
+                throw new ErrorHandler(400, 'Id not found');
+            }
+
             await task.destroy();
 
             return res.send({ message: 'Task was deleted!' });
-
         } catch (err) {
-            return res.status(404).send({ error: 'Task not found!' });
+            next(err);
         }
 });
 
