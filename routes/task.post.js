ac4ec6abb3c1296f0e1ca215951d3d5ff1e9b184
Ilya Kuznetsov <i.kuzntesov.w@gamil.com>
2021-06-04 10:46:56 +0300

    Add Error Handler

--- routes/task.post.js
+++ routes/task.post.js
@@ -1,13 +1,14 @@
 const { Router } = require ('express');
 const { Task } = require ('../models');
 const { body, validationResult } = require ('express-validator');
+const { ErrorHandler } = require('../error');
 
 const router = Router();
 
 router.post('/task',
     body('name').isString().isLength({ min: 1 }),
     body('done').isBoolean(),
-    async (req, res) => {
+    async (req, res, next) => {
         
         const { name, done } = req.body;
     
@@ -15,14 +16,22 @@ router.post('/task',
         const errors = validationResult(req);
 
         if (!errors.isEmpty()) {
-            return res.status(400).send(errors.array());
+            throw new ErrorHandler(400, errors.array());
         }
-
-        const task = await Task.create({ name, done });
-        return res.json(task); 
         
+        const taskName = await Task.findOne({
+            where: { name }
+        })
+        
+        if (taskName) {
+            throw new ErrorHandler(400, 'A task with this name exists');
+        }
+        
+        const task = await Task.create({ name, done });
+
+        return res.json(task); 
     } catch (err) {
-        return res.status(400).send(err);
+        next(err);
     }
 })
 
