64f01058cca87ebbf9cce32ca9cdfd938c736c6a
Ilya Kuznetsov <i.kuzntesov.w@gamil.com>
2021-06-04 15:47:30 +0300

    Change config and add dotenv

---
+++ config/config.js
@@ -0,0 +1,28 @@
+require('dotenv').config();
+
+const { DB_USER, DB_PASS, DB_NAME, DB_HOST } = process.env;
+
+module.exports = {
+  "development": {
+    "username": DB_USER,
+    "password": DB_PASS,
+    "database": DB_NAME,
+    "host": DB_HOST,
+    "dialect": "postgres"
+  },
+  "test": {
+    "username": DB_USER,
+    "password": DB_PASS,
+    "database": DB_NAME,
+    "host": DB_HOST,
+    "dialect": "postgres"
+  },
+  "production": {
+    "username": process.env.DB_USER_PROD,
+    "password": process.env.DB_PASS_PROD,
+    "database": process.env.DB_NAME_PROD,
+    "host": process.env.DB_HOST_PROD,
+    "port": process.env.DB_PORT_PROD,
+    "dialect": "postgres"
+  }
+}
