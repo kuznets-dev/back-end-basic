a405cd27c44794bfeb0801a38c60daec7fd0e9f6
Ilya Kuznetsov <i.kuzntesov.w@gamil.com>
2021-06-04 11:13:33 +0300

    Add Error Handler

--- routes/task.patch.js
+++ routes/task.patch.js
@@ -1,6 +1,7 @@
 const { Router } = require ('express');
 const { Task } = require ('../models');
 const { body, param, validationResult } = require ('express-validator');
+const { ErrorHandler } = require('../error');
 
 const router = Router();
 
@@ -8,7 +9,7 @@ router.patch('/task/:uuid',
     body('name').isString().isLength({ min: 1 }),
     body('done').isBoolean(),
     param('uuid').isUUID(),
-    async (req, res) => {
+    async (req, res, next) => {
 
         const uuid = req.params.uuid;
         const { name, done } = req.body;
@@ -17,10 +18,16 @@ router.patch('/task/:uuid',
             const errors = validationResult(req);
 
             if (!errors.isEmpty()) {
-                return res.status(400).send(errors.array());
+                throw new ErrorHandler(400, errors.array());
             }
 
-            const task = await Task.findOne({ where: {uuid} });
+            const task = await Task.findOne({
+                where: { uuid }
+            });
+
+            if (!task) {
+                throw new ErrorHandler(422, 'Task not found');
+            }
 
             task.name = name;
             task.done = done;
@@ -28,9 +35,8 @@ router.patch('/task/:uuid',
             await task.save();
 
             return res.send({ message: 'Task was changed!' });
-
         } catch (err) {
-            return res.status(404).send({ error: 'Task not found!' });
+            next(err);
         }
 })
 
