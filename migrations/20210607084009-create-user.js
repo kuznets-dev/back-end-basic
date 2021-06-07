ea3e53aa7b649388301d15de1f283610f090212b
Ilya Kuznetsov <i.kuzntesov.w@gamil.com>
2021-06-07 11:51:45 +0300

    Create migration

---
+++ migrations/20210607084009-create-user.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const { DataTypes } = require("sequelize/types");
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('users', {
+      uuid: {
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4,
+        primaryKey: true,
+        allowNull: false
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      password: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('users');
+  }
+};
