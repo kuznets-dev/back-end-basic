029760d7dd282aa2488691ce78bee7e093bef491
Ilya Kuznetsov <i.kuzntesov.w@gamil.com>
2021-06-07 12:15:35 +0300

    Add a new field

--- models/task.js
+++ models/task.js
@@ -24,6 +24,10 @@ module.exports = (sequelize, DataTypes) => {
     done: {
       type: DataTypes.BOOLEAN,
       allowNull: false
+    },
+    userUUID: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4
     }
   }, {
     sequelize,
