5e1bdf14f48ce09c2bf78c4c05717fd7448ac4ec
Ilya Kuznetsov <i.kuzntesov.w@gamil.com>
2021-06-07 17:38:53 +0300

    Change request

--- routes/tasks/task.delete.js
+++ routes/tasks/task.delete.js
@@ -2,10 +2,12 @@ const { Router } = require ('express');
 const { Task } = require ('../../models');
 const { param, validationResult } = require ('express-validator');
 const { ErrorHandler } = require('../../error');
+const authMiddleware = require('../../middleware/authMiddleware');
 
 const router = Router();
 
 router.delete('/task/:uuid',
+    authMiddleware,
     param('uuid').isUUID(),
     async (req, res, next) => {
 
@@ -18,15 +20,19 @@ router.delete('/task/:uuid',
                 throw new ErrorHandler(400, errors.array());
             }
 
+            const user_uuid = res.locals.user.uuid;
+
             const task = await Task.findOne({
-                where: { uuid }
+                where: { uuid, user_uuid }
             });
 
             if (!task) {
                 throw new ErrorHandler(400, 'Id not found');
             }
 
-            await task.destroy();
+            await task.destroy({
+                where: { uuid, user_uuid }
+            });
 
             return res.send({ message: 'Task was deleted!' });
         } catch (err) {
