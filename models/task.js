8e3cc694aa299e14fc54c80e2ed6de53d0d37990
Ilya Kuznetsov <i.kuzntesov.w@gamil.com>
2021-06-07 17:19:32 +0300

    Change field

--- models/task.js
+++ models/task.js
@@ -25,7 +25,7 @@ module.exports = (sequelize, DataTypes) => {
       type: DataTypes.BOOLEAN,
       allowNull: false
     },
-    userUUID: {
+    user_uuid: {
       type: DataTypes.UUID,
       defaultValue: DataTypes.UUIDV4
     }
