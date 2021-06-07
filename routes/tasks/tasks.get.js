09aaf5d6198c501d947547e889bb2358831bcf42
Ilya Kuznetsov <i.kuzntesov.w@gamil.com>
2021-06-07 17:21:37 +0300

    Change request

--- routes/tasks/tasks.get.js
+++ routes/tasks/tasks.get.js
@@ -2,10 +2,12 @@ const { Router } = require ("express");
 const { Task } = require ('../../models');
 const { ErrorHandler } = require ('../../error')
 const { query, validationResult } = require ('express-validator');
+const authMiddleware = require('../../middleware/authMiddleware');
 
 const router = Router();
 
-router.get('/tasks', 
+router.get('/tasks',
+    authMiddleware,
     query('filterBy').isString().optional({ checkFalsy: true }),
     query('orderBy').isString().optional({ checkFalsy: true }),
     query('page').isInt().optional({ checkFalsy: true }),
@@ -13,10 +15,11 @@ router.get('/tasks',
     async (req, res, next) => {
         try {
             const errors = validationResult(req);
-
             if (!errors.isEmpty()) {
                 throw new ErrorHandler(400, errors.array());
             }
+
+            const user_uuid = res.locals.user.uuid;
 
             const { filterBy = '', orderBy = 'asc', page = 1, limit = 5 } = req.query;
 
@@ -29,13 +32,13 @@ router.get('/tasks',
 
             const {count, rows} = await Task.findAndCountAll({
                 where: {
+                    user_uuid: user_uuid,
                     done: filteredTasks[filterBy]
                 },
                 order: [['createdAt', sorteredTasks[orderBy]]],
                 limit: limit,
                 offset: (page - 1) * limit
             })
-
             const pageCount =  Math.ceil(count / limit);
             
             return res.send({pageCount, rows});
