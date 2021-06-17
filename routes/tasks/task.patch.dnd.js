8d17b3ecf88a2d293dfeb1813b5d3f238e99844c
Ilya Kuznetsov <i.kuzntesov.w@gamil.com>
2021-06-17 18:50:32 +0300

    Add logic for dnd

--- routes/tasks/task.patch.dnd.js
+++ routes/tasks/task.patch.dnd.js
@@ -1,29 +1,51 @@
 const { Router } = require('express');
+const { Op } = require("sequelize");
 const { Task } = require('../../models');
-const { body, param, validationResult } = require('express-validator');
+const { body, validationResult } = require('express-validator');
 const { ErrorHandler } = require('../../error');
 const authMiddleware = require('../../middleware/authMiddleware');
 
 const router = Router();
 
-router.patch('/dnd', 
+router.patch('/dnd',
     authMiddleware,
-    body('source').isInt(),
-    body('destination').isInt(),
+    body('source').isObject(),
+    body('destination').isObject(),
     async (req, res, next) => {
         const { source, destination } = req.body;
-        console.log(source, destination);
+        const user_uuid = res.locals.user.uuid;
+        const desIndex = destination.index
+        const sourIndex = source.index
+        const sourUUID = source.uuid
 
         try {
             const errors = validationResult(req);
+
             if (!errors.isEmpty()) {
                 throw new ErrorHandler(400, 'Invalid request', errors.array());
             }
 
+            if (sourIndex < desIndex) {
+                const betweenTasks = await Task.findAll({
+                    where: {
+                        user_uuid,
+                        index: { [Op.between]: [sourIndex, desIndex] }
+                    }
+                })
+                await betweenTasks.map(task => Task.update({ index: task.index - 1 }, { where: { uuid: task.uuid, user_uuid } }));
+            } else {
+                const betweenTasks = await Task.findAll({
+                    where: {
+                        user_uuid,
+                        index: { [Op.between]: [desIndex, sourIndex] }
+                    }
+                })
+                await betweenTasks.map(task => Task.update({ index: task.index + 1 }, { where: { uuid: task.uuid, user_uuid } }))
+            }
 
-            
+            await Task.update({ index: desIndex }, { where: { uuid: sourUUID, user_uuid } })
+            return res.send();
         } catch (err) {
-            console.log(err);
             next(err);
         }
     }
